ed5829f34d34fa782b76314fdf47e65096756ce6
dlepke <deannalepke@gmail.com>
2023-06-07 13:48:26 -0700

    basic skeleton

---
+++ public/js/nav.js
@@ -0,0 +1,11 @@
+let html =
+  '<ul>\
+  <li><a href="/">Home</a></li>\
+  <li><a href="/leetcode">Leetcode</a></li>\
+  <li><a href="/machinelearning">Machine Learning</a></li>\
+  <li><a href="/pokemon">Pokemon</a></li>\
+  <li><a href="/apps">Apps</a></li>\
+  <li><a href="/contact">Contact</a></li>\
+</ul>';
+
+document.getElementById("nav").innerHTML = html;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/index.html"));
+});
+
+app.get("/pokemon", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/pokemon.html"));
+});
+
+app.get("/leetcode", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/leetcode.html"));
+});
+
+app.get("/machinelearning", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/machinelearning.html"));
+});
+
+app.get("/apps", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/apps.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/contact.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
