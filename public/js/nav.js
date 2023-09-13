1e9b515aedc441e919c796f2a3d06fc56cd60924
dlepke <deannalepke@gmail.com>
2023-09-12 20:04:02 -0700

    undid silly html comment from previous commit

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,13 +1,13 @@
 let html =
   '<ul>\
   <li><a href="/">Home</a></li>\
-  /*<li><a href="/leetcode">Leetcode</a></li>\
+  <li><a href="/leetcode">Leetcode</a></li>\
   <li><a href="/machinelearning">Machine Learning</a></li>\
   <li><a href="/pokemon">Pokemon</a></li>\
   <li><a href="/apps">Apps</a></li>\
   <li><a href="/timeline">Timeline</a></li>\
   <li><a href="/resume">Resume</a></li>\
-  <li><a href="/contact">Contact</a></li>*/\
+  <li><a href="/contact">Contact</a></li>\
 </ul>';
 
 document.getElementById("nav").innerHTML = html;
