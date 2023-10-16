3f7d9accfe78b3103fa558f50e7f370739c41ce3
dlepke <deannalepke@gmail.com>
2023-10-16 15:54:27 -0700

    reduced scope and removed unused files

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,12 +1,8 @@
 let html =
   '<ul>\
   <li><a href="/">Home</a></li>\
-  <li><a href="/leetcode">Leetcode</a></li>\
-  <li><a href="/machinelearning">Machine Learning</a></li>\
-  <li><a href="/pokemon">Pokemon</a></li>\
-  <li><a href="/apps">Apps</a></li>\
-  <li><a href="/timeline">Timeline</a></li>\
   <li><a href="/resume">Resume</a></li>\
+  <li><a href="/projects">Projects</a></li>\
   <li><a href="/contact">Contact</a></li>\
 </ul>';
 
--- server.js
+++ server.js
@@ -19,28 +19,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/html/index.html"));
 });
 
-app.get("/pokemon", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/html/pokemon.html"));
-});
-
-app.get("/leetcode", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/html/leetcode.html"));
-});
-
-app.get("/machinelearning", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/html/machinelearning.html"));
-});
-
-app.get("/apps", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/html/apps.html"));
-});
-
-app.get("/timeline", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/html/timeline.html"));
-});
-
 app.get("/resume", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/html/resume.html"));
+});
+
+app.get("/projects", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/projects.html"));
 });
 
 app.get("/contact", (req, res) => {
